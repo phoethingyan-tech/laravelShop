89e30eaf6bcdf9820eb4768948c323ee4ff1b084
phoethingyan-tech <phoethingyan.tech@gmail.com>
2025-05-11 14:28:35 +0630

    update admin panel

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\FrontController::class, 'shop'])->name('shop');
 Route::get('/shop-item/{id}', [App\Http\Controllers\FrontController::class, 'shopItem'])->name('shop-item');
 
+Route::group(['prefix'=>'backend', 'as'=>'backend.'], function() {
+    Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'dashboard'])->name('dashboard');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
