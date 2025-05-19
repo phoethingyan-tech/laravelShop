182e4e56180e5865bf9b0343d2ee92ea782153e6
phoethingyan-tech <phoethingyan.tech@gmail.com>
2025-05-19 16:05:16 +0630

    data collect

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class FrontController extends Controller
 {
     public function shop()
     {
-        return view('front.shops');
+        $items = Item::all();
+        // var_dump($items);
+        return view('front.shops',compact('items')); 
     }
 
     public function shopItem($id) 
     {
-        return view('front.shop-item');
+        // echo $id;
+        $item = Item::find($id);
+        // var_dump($item);
+        return view('front.shop-item',compact('item'));
     }
 }
